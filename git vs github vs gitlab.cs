0452d5800b2776fbf55f8a1d90144fa2a3f95333
JacklenJames-24 <jamesjacklen24@gmail.com>
2024-07-01 07:59:31 +0530

    Initial commit

---
+++ git vs github vs gitlab.cs
@@ -0,0 +1,9 @@
+ git vs github vs gitlab
+ git commands
+ push code portfolio
+ Solid principles
+ Oops concepts (class object static abstractv interface polymorphissm, abstraction encapsulation, inheritance)
+ Php basics
+
+ namaste javascript
+ 
